3e9205f21cb7a868f73213f9f73f18ce033e074c
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-09-24 18:04:23 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
 import {react, useState} from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/classCounter';
+import './styles/App.css';
+import Postitem from './components/Postitem';
 
 function App() {
-  const [value, setValue] = useState('Text')
 
   return (
     <div className="App">
-      <ClassCounter/>
+      <Postitem post={{id: 1, title: 'Javascript', body: 'Decription'}}/>
+      <Postitem post={{id: 2, title: 'HTML', body: 'Decription'}}/>
+      <Postitem post={{id: 3, title: 'CSS', body: 'Decription'}}/>
+      <Postitem post={{id: 4, title: 'Python', body: 'Decription'}}/>
+      <Postitem post={{id: 5, title: 'React', body: 'Decription'}}/>
+    
     </div>
   );
 }
---
+++ src/components/Postitem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Postitem = (props) => {
+    return (
+        <div>
+            <div className="post">
+        <div className="post__content">
+          <strong>{props.post.id}. {props.post.title}</strong>
+          <div>
+            {props.post.body}
+          </div>
+          <div className="post__buttons">
+            <button>Delete</button>
+          </div>
+        </div>
+      </div>
+        </div>
+    )
+}
+
+export default Postitem;
