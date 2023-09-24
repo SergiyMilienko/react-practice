ae8760907056424fdefa4fbcdb1b9bbc551cb704
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-09-24 16:45:53 +0300

    ClassComponents

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import {react, useState} from 'react';
 import Counter from './components/Counter';
+import ClassCounter from './components/classCounter';
 
 function App() {
   const [value, setValue] = useState('Text')
 
   return (
     <div className="App">
-      <Counter/>
-      <Counter/>
-      <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
