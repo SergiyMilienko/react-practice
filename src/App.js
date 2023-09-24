84ef9a033491d6d4cc212c582314bebbe04f06a9
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-09-24 18:11:43 +0300

    props2

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import './styles/App.css';
 import Postitem from './components/Postitem';
 
 function App() {
-
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Decription'},
+    {id: 2, title: 'HTML', body: 'Decription'},
+    {id: 3, title: 'CSS', body: 'Decription'},
+    {id: 4, title: 'Python', body: 'Decription'},
+    {id: 5, title: 'React', body: 'Decription'}
+  ])
   return (
     <div className="App">
-      <Postitem post={{id: 1, title: 'Javascript', body: 'Decription'}}/>
-      <Postitem post={{id: 2, title: 'HTML', body: 'Decription'}}/>
-      <Postitem post={{id: 3, title: 'CSS', body: 'Decription'}}/>
-      <Postitem post={{id: 4, title: 'Python', body: 'Decription'}}/>
-      <Postitem post={{id: 5, title: 'React', body: 'Decription'}}/>
-    
+      <h1 style={{textAlign: 'center'}}>List of posts</h1>
+      {posts.map(post => 
+        <Postitem post={post} key={post.id}/>
+      )}
     </div>
   );
 }
